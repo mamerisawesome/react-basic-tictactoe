abc0bbe8f3e392a9586a55cdf9acf88d247ce862
amendoza <amendoza@stratpoint.com>
2020-02-22 18:11:20 +0800

    Added static files

---
+++ precache-manifest.b2afb274b7e100112f5d31a34803c516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6f5707ea9450b01de1948959d6e647",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d729a229b20510ec1ce5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "191fe7cd63b4fbf31872",
+    "url": "/static/js/2.df0e5a58.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.df0e5a58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d729a229b20510ec1ce5",
+    "url": "/static/js/main.f27d3f5a.chunk.js"
+  },
+  {
+    "revision": "b2362c981b140383cafc",
+    "url": "/static/js/runtime-main.abcc6591.js"
+  }
+]);
